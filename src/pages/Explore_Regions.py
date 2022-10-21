d613f3479a19f94479f944cebbd1534c7f90a71b
citlali-trigos-raczkowski <cbtrigos@gmail.com>
2022-10-21 21:45:43 +0300

    feat(explore-regions): bulking up

--- src/pages/Explore_Regions.py
+++ src/pages/Explore_Regions.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from millify import prettify
 
 # TODO: get the data by uncommenting the below line (imports not working for me rn)
 # from '../Home' import get_df
@@ -16,7 +17,8 @@ def get_data():
 
 df = get_data().rename(columns={"X": "lon", "Y": "lat"})
 
-df['date'] = df[['Incident year','Reported Month']].apply(lambda x: '-'.join(x.values.astype(str)), axis="columns")
+df['date'] = df[['Incident year', 'Reported Month']].apply(
+    lambda x: '-'.join(x.values.astype(str)), axis="columns")
 df['date'] = df['date'] + '-1'
 df['date'] = pd.to_datetime(df['date'])
 migrantdf = df
@@ -25,11 +27,11 @@ df1 = df.groupby(['Migration route', 'Season', 'Incident year'])[
     'Total Number of Dead and Missing'].sum().reset_index(name='count')
 df2 = df.groupby(['Migration route', 'Cause of Death', 'Cause of Death Abbreviation'])[
     'Total Number of Dead and Missing'].sum().reset_index(name='Total Number of Dead and Missing')
-df_w_cause = df.groupby(['Migration route','Cause of Death','date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
-                                                                          'Number of Females', 'Number of Males', 'Number of Children', 'Number of Survivors']].sum().reset_index()
+df_w_cause = df.groupby(['Migration route', 'Cause of Death', 'date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
+                                                                        'Number of Females', 'Number of Males', 'Number of Children', 'Number of Survivors']].sum().reset_index()
 
-df_wout_cause = df.groupby(['Migration route','date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
-                                                                          'Number of Females', 'Number of Males', 'Number of Children', 'Number of Survivors']].sum().reset_index()
+df_wout_cause = df.groupby(['Migration route', 'date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
+                                                        'Number of Females', 'Number of Males', 'Number of Children', 'Number of Survivors']].sum().reset_index()
 
 # Functions to create the graphs
 
@@ -39,44 +41,49 @@ def plot_deaths_season(m_route, df):
     dft = df[df['Migration route'] == m_route]
 
     fig = px.line(dft, x='Incident year', y='count',
-                  color='Season', title=f"Total deaths per Season in {m_route}")
+                  color='Season', title=f"Total deaths per season in {m_route}")
     fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'xaxis_title': "",
     })
     st.write(fig)
 
 
-def plot_deaths_month(m_route,cause, df_wout_cause,df_wcause):
+def plot_deaths_month(m_route, cause, df_wout_cause, df_wcause):
     if len(cause) > 0:
         dft = df_wcause[(df_wcause['Migration route'] == m_route) & (df_wcause['Cause of Death'].isin(
-        cause))].groupby(['Migration route','date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
-                                                                          'Number of Females', 'Number of Males', 'Number of Children']].sum().reset_index()
-    else: 
+            cause))].groupby(['Migration route', 'date'])[['Total Number of Dead and Missing', 'Minimum Estimated Number of Missing',
+                                                           'Number of Females', 'Number of Males', 'Number of Children']].sum().reset_index()
+    else:
         dft = df_wout_cause[df_wout_cause['Migration route'] == m_route]
-    fig = px.line(dft, x='date', y=['Total Number of Dead and Missing','Number of Females', 'Number of Males', 'Number of Children'], title=f"Plot of deaths by month along the {m_route} route.")
+    fig = px.line(dft, x='date', y=['Total Number of Dead and Missing', 'Number of Females',
+                  'Number of Males', 'Number of Children'], title=f"Plot of deaths by month along the {m_route} route.")
     fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-        'xaxis_title':"Date", 
-        'yaxis_title':"Number of Deaths",
+        'xaxis_title': "",
+        'yaxis_title': "Number of deaths",
     })
     st.write(fig)
 
-def plot_deaths_and_survivors_month(m_route,cause, df_wout_cause,df_wcause):
+
+def plot_deaths_and_survivors_month(m_route, cause, df_wout_cause, df_wcause):
     if len(cause) > 0:
         dft = df_wcause[(df_wcause['Migration route'] == m_route) & (df_wcause['Cause of Death'].isin(
-        cause))].groupby(['Migration route','date'])[['Total Number of Dead and Missing', 'Number of Survivors']].sum().reset_index()
-    else: 
+            cause))].groupby(['Migration route', 'date'])[['Total Number of Dead and Missing', 'Number of Survivors']].sum().reset_index()
+    else:
         dft = df_wout_cause[df_wout_cause['Migration route'] == m_route]
-    fig = px.line(dft, x='date', y=['Total Number of Dead and Missing','Number of Survivors'], title=f"Plot of deaths and survivors by month along the {m_route} route." )
+    fig = px.line(dft, x='date', y=['Total Number of Dead and Missing', 'Number of Survivors'],
+                  title=f"Plot of deaths and survivors by month along the {m_route} route.")
     fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-        'xaxis_title':"Date", 
-        'yaxis_title':"Number of Deaths",
+        'xaxis_title': "",
+        'yaxis_title': "Number of deaths",
     })
     st.write(fig)
+
 
 def plot_deaths_cause(m_route, causes, df):
     dft = df[df['Migration route'] == m_route].sort_values(
@@ -90,14 +97,15 @@ def plot_deaths_cause(m_route, causes, df):
             except:
                 pass
         fig = px.bar(dft, x='Cause of Death Abbreviation', y='Total Number of Dead and Missing',
-                     title=f"Cause of Death in {m_route}", color=colors)
+                     title=f"Causes of death in {m_route}", color=colors)
     else:
         fig = px.bar(dft, x='Cause of Death Abbreviation', y='Total Number of Dead and Missing',
-                     title=f"Cause of Death in {m_route}")
+                     title=f"Causes of death in {m_route}")
 
     fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'xaxis_title': "",
     })
 
     st.write(fig)
@@ -107,7 +115,7 @@ def plot_comp(m_route, cause, df):
     dft = df[(df['Migration route'].isin(m_route)) & (df['Cause of Death'].isin(
         cause))].sort_values(by='Total Number of Dead and Missing', ascending=False)
     fig = px.bar(dft, x='Cause of Death Abbreviation', y='Total Number of Dead and Missing', color='Migration route', barmode='group',
-                 title="Number of Dead by Cause of Death")
+                 title="Number of Deaths by cause of death")
 
     fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
@@ -119,11 +127,70 @@ def plot_comp(m_route, cause, df):
 
 #  Markdown for the page
 st.header("Explore One Region at a Time")
-st.markdown("In this page we welcome you to explore the data one region at a time, using the filters in the left side menu to filter. Try using the filters and check how the data visualization changes." +
-"Migrations that cross borders and routes include entire families with children. Those routes are so dangerous, and even more for infants." + "On the other hand, others manage to survive the journey, but the fact that they do does not mean that it was without great difficulty and that many others did not.")
+st.markdown("In this page we welcome you to explore the data one region at a time, using the filters in the left side menu. Try selecting a new region to check how the data visualization changes. " +
+            "Migrations that cross borders and routes often include families with children. While extremely dangerous, they are even moreso for infants." + " For those that do survive the journey, it is not without great difficulty and loss.")
+
+# TODO: include list of most deadly regions 
+
 st.sidebar.write("Data Filters")
+
 route_input = [st.sidebar.selectbox(
     'Migration Route', df['Migration route'].unique().tolist())]
+st.header('Exploring the '+route_input[0])
+
+
+if route_input:
+    route_s = route_input[0]
+    mdf = df[df['Migration route'] == route_s]
+    migrantdf = mdf.groupby(['Cause of Death'])[
+        'Total Number of Dead and Missing'].sum().reset_index().sort_values(by='Total Number of Dead and Missing', ascending=False)
+    route_s = route_input[0]
+    worst_month = mdf.groupby(['Migration route', 'date'])['Total Number of Dead and Missing'].sum(
+    ).reset_index().sort_values(by='Total Number of Dead and Missing', ascending=False)
+    df_d_s = mdf.groupby(['Migration route'])[
+        ['Total Number of Dead and Missing', 'Number of Survivors', 'Number of Females', 'Number of Males', 'Number of Children']].sum().reset_index()
+
+    total_dead_missing = prettify(
+        df_d_s['Total Number of Dead and Missing'].iloc[0])
+    total_women_dead_missing = prettify(df_d_s['Number of Females'].iloc[0])
+    total_men_dead_missing = prettify(df_d_s['Number of Males'].iloc[0])
+    total_children_dead_missing = prettify(
+        df_d_s['Number of Children'].iloc[0])
+
+    # introduction
+    st.markdown("There are a total of  " + total_dead_missing +
+                " recorded deaths in the data since 2014 for the " + route_input[0] +
+                " migration route. This includes " + total_women_dead_missing +
+                " women, " + total_men_dead_missing + " men, and " + total_children_dead_missing + " children.")
+
+    # TODO: origination data
+    st.markdown("Most of the migrants taking this route originate from ..")
+
+    # map of the selection region
+    st.map(mdf)
+
+    #   column statistics
+    st.subheader("View the Statistics")
+    col1, col2 = st.columns(2)
+    cause1 = migrantdf['Cause of Death'].iloc[0]
+    st.metric(f'Most common cause of death', cause1)
+    cause2 = migrantdf['Cause of Death'].iloc[1]
+    st.metric(f'Second most common cause of death', cause2)
+
+    with col1:
+        st.metric(f'Total number of Dead and Missing since 2014',
+                  total_dead_missing)
+    with col2:
+        st.metric(f'Number of Recorded Survivors',
+                  prettify(df_d_s['Number of Survivors'].iloc[0]))
+    col3, col4 = st.columns(2)
+    with col3:
+        st.metric(f'Deadliest event date',
+                  worst_month['date'].dt.strftime('%Y-%m').iloc[0])
+    with col4:
+        st.metric(f'Deadliest event number of deaths',
+                  prettify(worst_month['Total Number of Dead and Missing'].iloc[0]))
+
 
 cause_of_death_input = st.sidebar.multiselect(
     'Cause of Death', df['Cause of Death'].unique().tolist())
@@ -131,42 +198,18 @@ if len(cause_of_death_input) > 0:
     migrantdf = df[df['Cause of Death'].isin(
         cause_of_death_input)]
 
+st.markdown('\n\n\n')
+st.subheader("Maps of Death by Year, Month, and Season")
+
+st.markdown("Each of the following graphs can be expanded using the expanding arrows button that appears to the right of the graph on hover. The graphs are interactive and scalable.")
 
 if route_input:
-    route_s= route_input[0]
-    plot_deaths_month(route_s,cause_of_death_input,df_wout_cause,df_w_cause)
-    plot_deaths_and_survivors_month(route_s,cause_of_death_input,df_wout_cause,df_w_cause)
+
+    plot_deaths_month(route_s, cause_of_death_input, df_wout_cause, df_w_cause)
+    plot_deaths_and_survivors_month(
+        route_s, cause_of_death_input, df_wout_cause, df_w_cause)
     plot_deaths_season(route_s, df1)
-    plot_deaths_cause(route_s,cause_of_death_input, df2)
+    plot_deaths_cause(route_s, cause_of_death_input, df2)
 
 if len(route_input) > 0 and len(cause_of_death_input) > 0:
     plot_comp(route_input, cause_of_death_input, df2)
-
-
-
-if route_input:
-
-    mdf = df[df['Migration route'] == route_s]
-    migrantdf = mdf.groupby(['Cause of Death'])[
-    'Total Number of Dead and Missing'].sum().reset_index().sort_values(by='Total Number of Dead and Missing',ascending=False)
-    cause1 = migrantdf['Cause of Death'].iloc[0]
-    st.metric(f'Most common cause of death',cause1)
-    if cause1 =='Mixed or unknown':
-        cause2= migrantdf['Cause of Death'].iloc[1]
-        st.metric(f'Second most common cause of death',cause2)
-
-    
-    col1,col2= st.columns(2)
-    route_s= route_input[0]
-    worst_month = mdf.groupby(['Migration route','date'])['Total Number of Dead and Missing'].sum().reset_index().sort_values(by='Total Number of Dead and Missing',ascending=False)
-    df_d_s = mdf.groupby(['Migration route'])[['Total Number of Dead and Missing','Number of Survivors']].sum().reset_index()
-    
-    with col1:
-        st.metric(f'Total number of Dead and Missing',df_d_s['Total Number of Dead and Missing'].iloc[0])
-    with col2:
-        st.metric(f'Number of Survivors',df_d_s['Number of Survivors'].iloc[0])
-    col3,col4 = st.columns(2)
-    with col3:
-        st.metric(f'Deadliest event date',worst_month['date'].dt.strftime('%Y-%m').iloc[0])
-    with col4:
-        st.metric(f'Deadliest event number of deaths',worst_month['Total Number of Dead and Missing'].iloc[0])
