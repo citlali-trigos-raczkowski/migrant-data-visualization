8ace9851f46169790cdf6096ecdbe100b8d8c539
kuurasuo <109953594+kuurasuo@users.noreply.github.com>
2022-10-23 17:12:32 +0300

    Update Tweet_Processing.py

--- Tweet_Processing.py
+++ Tweet_Processing.py
@@ -81,7 +81,6 @@ print(unigrams.head(20))
 print(bigrams.head(20))
 print(trigrams.head(20))
 
-print(df['Date'].head())
 dm = []
 dates = []
 df2 = pd.DataFrame(columns = ['Date', 'Tweet'])
@@ -91,62 +90,41 @@ for tweet, date in zip(df['Tweet'], df['Date']):
         if 'dead' in tweet or 'died' in tweet or 'death' in tweet or 'bodies' in tweet or 'missing' in tweet:
             dm.append(tweet)
             dates.append(date)
-print(dm[0])
-print(dates[0])
 
-#for tweet in dm:
-#    l = tweet.split()
-#    for i in range(len(l)):
-#        s = l[i].strip(',.:;')
-#        st = ''
-#        if s == 'dead' or s == 'died' or s == 'death' or s == 'bodies':
-#            st += 'dead : ' + str(l[i-3]) + ' ' + str(l[i-2]) + ' ' + str(l[i-1]) + ' ' + str(l[i])
-#        elif s == 'missing':
-##            st += 'missing : ' + str(l[i-3]) + ' ' + str(l[i-2]) + ' ' + str(l[i-1]) + ' ' + str(l[i])
-#        if st != '':
-#            strs.append(st)
-
-print(len(dm))
-print(len(dates))
 for t,d in zip(dm, dates):
     print('Tweet: ' + t + ' Date: ' + d)
 
-# It appears to be very difficult to distiguish separate incidents since the reports do not include dates of occurence, and also 
-# between incidents that occur migratory journeys and those that occur to migrants in general. The incidents are rare enough 
-# that the amount of tweets after initial processing is only 95, and thus there is not enough return on investment to create a 
-# language processing model to get the data, so instead I'll get data manually from the tweets.
-
 data_list = [ 
-    {'route' : 'Central Mediterranian', 'location' : 'Mahdia coast, Tunisia', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-14'},
-    {'route' : 'Central Mediterranian', 'location' : 'Trapani, Sicily', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-10-13'},
-    {'route' : 'Central Mediterranian', 'location' : 'Zarzis, Tunisia', 'number dead' : '11', 'number missing' : '0', 'date' : '2022-09-21'},
-    {'route' : 'Central Mediterranian', 'location' : 'Sbaratha, Libya', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-07'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Kythira, Greece', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-10-05'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Lesbos', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-10-06'},
-    {'route' : 'Western Mediterranian', 'location' : 'Spain / Canary Islands', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-10-06'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Syria', 'number dead' : '97', 'number missing' : '0', 'date' : '2022-09-26'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Aegean Sea, Turkey', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-15'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Turkey, coast of Izmir', 'number dead' : '6', 'number missing' : '5', 'date' : '2022-09-15'},
-    {'route' : 'Western Mediterranian', 'location' : 'Morocco', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-13'},
-    {'route' : 'Central Mediterranian', 'location' : 'Pozzallo, Sicily', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-10'},
-    {'route' : 'Central Mediterranian', 'location' : 'Tunisia', 'number dead' : '8', 'number missing' : '15', 'date' : '2022-09-09'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Crete, Greece', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-08'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Lebanon', 'number dead' : '30', 'number missing' : '0', 'date' : '2022-04-24'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Turkey', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-08-26'},
-    {'route' : 'Western Mediterranian', 'location' : 'Canary Islands, Fuerteventura', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-08-16'},
-    {'route' : 'Central Mediterranian', 'location' : 'Libya, Sudan border', 'number dead' : '15', 'number missing' : '2', 'date' : '2022-08-15'},
-    {'route' : 'Western Mediterranian', 'location' : 'Algerian coast', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-08-09'},
-    {'route' : 'Western Mediterranian', 'location' : 'Morocco, Akhfennir', 'number dead' : '8', 'number missing' : '0', 'date' : '2022-07-26'},
+    {'route' : 'Central Mediterranean', 'location' : 'Mahdia coast, Tunisia', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-14'},
+    {'route' : 'Central Mediterranean', 'location' : 'Trapani, Sicily', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-10-13'},
+    {'route' : 'Central Mediterranean', 'location' : 'Zarzis, Tunisia', 'number dead' : '11', 'number missing' : '0', 'date' : '2022-09-21'},
+    {'route' : 'Central Mediterranean', 'location' : 'Sbaratha, Libya', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-07'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Kythira, Greece', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-10-05'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Lesbos', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-10-06'},
+    {'route' : 'Western Mediterranean', 'location' : 'Spain / Canary Islands', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-10-06'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Syria', 'number dead' : '97', 'number missing' : '0', 'date' : '2022-09-26'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Aegean Sea, Turkey', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-15'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Turkey, coast of Izmir', 'number dead' : '6', 'number missing' : '5', 'date' : '2022-09-15'},
+    {'route' : 'Western Mediterranean', 'location' : 'Morocco', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-13'},
+    {'route' : 'Central Mediterranean', 'location' : 'Pozzallo, Sicily', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-10'},
+    {'route' : 'Central Mediterranean', 'location' : 'Tunisia', 'number dead' : '8', 'number missing' : '15', 'date' : '2022-09-09'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Crete, Greece', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-08'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Lebanon', 'number dead' : '30', 'number missing' : '0', 'date' : '2022-04-24'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Turkey', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-08-26'},
+    {'route' : 'Western Mediterranean', 'location' : 'Canary Islands, Fuerteventura', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-08-16'},
+    {'route' : 'Central Mediterranan', 'location' : 'Libya, Sudan border', 'number dead' : '15', 'number missing' : '2', 'date' : '2022-08-15'},
+    {'route' : 'Western Mediterranean', 'location' : 'Algerian coast', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-08-09'},
+    {'route' : 'Western Mediterranean', 'location' : 'Morocco, Akhfennir', 'number dead' : '8', 'number missing' : '0', 'date' : '2022-07-26'},
     {'route' : 'Sahara Desert crossing', 'location' : 'Niger, Libyan border', 'number dead' : '10', 'number missing' : '0', 'date' : '2022-07-01'},
-    {'route' : 'Central Mediterranian', 'location' : 'Calabrian coast', 'number dead' : '5', 'number missing' : '0', 'date' : '2022-07-25'},
+    {'route' : 'Central Mediterranean', 'location' : 'Calabrian coast', 'number dead' : '5', 'number missing' : '0', 'date' : '2022-07-25'},
     {'route' : 'Sahara Desert crossing', 'location' : 'Libya, Chad border', 'number dead' : '20', 'number missing' : '0', 'date' : '2022-06-30'},
-    {'route' : 'Central Mediterranian', 'location' : 'Central Mediterranian', 'number dead' : '0', 'number missing' : '30', 'date' : '2022-06-27'},
+    {'route' : 'Central Mediterranean', 'location' : 'Central Mediterranean', 'number dead' : '0', 'number missing' : '30', 'date' : '2022-06-27'},
     {'route' : 'US-Mexico border crossing', 'location' : 'US, San Antonio', 'number dead' : '46', 'number missing' : '0', 'date' : '2022-06-28'},
-    {'route' : 'Central Mediterranian', 'location' : 'Morocco (to Mellilla)', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-06-28'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Lesbos, Greece', 'number dead' : '2', 'number missing' : '0', 'date' : '2022-06-27'},
-    {'route' : 'Central Mediterranian', 'location' : 'Tunisia, Coast of Sfax', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-06-21'},
-    {'route' : 'Eastern Mediterranian', 'location' : 'Greece, Aegean sea', 'number dead' : '0', 'number missing' : '8', 'date' : '2022-06-20'},
-    {'route' : 'Western Mediterranian', 'location' : 'Spain coast', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-06-09'},
+    {'route' : 'Central Mediterranean', 'location' : 'Morocco (to Mellilla)', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-06-28'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Lesbos, Greece', 'number dead' : '2', 'number missing' : '0', 'date' : '2022-06-27'},
+    {'route' : 'Central Mediterranean', 'location' : 'Tunisia, Coast of Sfax', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-06-21'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Greece, Aegean sea', 'number dead' : '0', 'number missing' : '8', 'date' : '2022-06-20'},
+    {'route' : 'Western Mediterranean', 'location' : 'Spain coast', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-06-09'},
     {'route' : 'Caribbean to US', 'location' : 'Ilsa Morada', 'number dead' : '0', 'number missing' : '1', 'date' : '2022-09-02'}]
 
 s = defaultdict(int)
@@ -159,7 +137,6 @@ for d in data_list:
 print(s)
 
 dataframe = pd.DataFrame.from_dict(data_list)
-print(dataframe.head())
 
 # save dataframe to csv
 #dataframe.to_csv('data.csv', index=False)
