2f5d09d24134a529e7e05e7209880fc1b2c4e5bd
kuurasuo <109953594+kuurasuo@users.noreply.github.com>
2022-10-26 00:05:55 +0300

    Add files via upload
    Cleaned the code further in Tweet_processing.py. Added coordinates of the approximate locations of the incidents from the dataset to data.csv, and using these beefed up the Twitter page with a similar map of incident locations as in the Home page.

--- Tweet_Processing.py
+++ Tweet_Processing.py
@@ -1,13 +1,11 @@
 """
-A script for processing tweets
+A script for processing Tweets
 """
 
 import numpy as np
 import pandas as pd
 import sklearn.feature_extraction.text as text
-import matplotlib.pyplot as plt
 import re
-from collections import defaultdict
 
 df1 = pd.read_csv("./data/sarw.csv")
 df2 = pd.read_csv("./data/info.csv")
@@ -59,31 +57,8 @@ def ng(t_input, max_features, ngram):
         index = tfidf.get_feature_names_out()
         ).sort_values(ascending = False).head(100)
 
-# printing the most common words and word combinations in the tweets to get an of what kind of wording is used in reports
-
-unigrams = ng(
-    t_input = tweetlist,
-    max_features = 5000,
-    ngram = 1
-    )
-bigrams = ng(
-    t_input = tweetlist,
-    max_features = 5000,
-    ngram = 2
-    )
-trigrams = ng(
-    t_input = tweetlist,
-    max_features = 5000,
-    ngram = 3
-    )
-
-print(unigrams.head(20))
-print(bigrams.head(20))
-print(trigrams.head(20))
-
 dm = []
 dates = []
-df2 = pd.DataFrame(columns = ['Date', 'Tweet'])
 
 for tweet, date in zip(df['Tweet'], df['Date']):
     if 'migrant' in tweet or 'migrants' in tweet:
@@ -91,52 +66,75 @@ for tweet, date in zip(df['Tweet'], df['Date']):
             dm.append(tweet)
             dates.append(date)
 
+unigrams = ng(
+    t_input = dm,
+    max_features = 5000,
+    ngram = 1
+    )
+bigrams = ng(
+    t_input = dm,
+    max_features = 5000,
+    ngram = 2
+    )
+trigrams = ng(
+    t_input = dm,
+    max_features = 5000,
+    ngram = 3
+    )
+
+# printing the most common words and word combinations in the tweets to get an idea of what kind of wording is used in reports
+print(unigrams.head(20))
+print(bigrams.head(20))
+print(trigrams.head(20))
+
+# print Tweets and dates for manual inspection
 for t,d in zip(dm, dates):
     print('Tweet: ' + t + ' Date: ' + d)
 
+# manually inputting data gathered by reading processed Tweets 
 data_list = [ 
-    {'route' : 'Central Mediterranean', 'location' : 'Mahdia coast, Tunisia', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-14'},
-    {'route' : 'Central Mediterranean', 'location' : 'Trapani, Sicily', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-10-13'},
-    {'route' : 'Central Mediterranean', 'location' : 'Zarzis, Tunisia', 'number dead' : '11', 'number missing' : '0', 'date' : '2022-09-21'},
-    {'route' : 'Central Mediterranean', 'location' : 'Sbaratha, Libya', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-07'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Kythira, Greece', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-10-05'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Lesbos', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-10-06'},
-    {'route' : 'Western Mediterranean', 'location' : 'Spain / Canary Islands', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-10-06'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Syria', 'number dead' : '97', 'number missing' : '0', 'date' : '2022-09-26'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Aegean Sea, Turkey', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-15'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Turkey, coast of Izmir', 'number dead' : '6', 'number missing' : '5', 'date' : '2022-09-15'},
-    {'route' : 'Western Mediterranean', 'location' : 'Morocco', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-13'},
-    {'route' : 'Central Mediterranean', 'location' : 'Pozzallo, Sicily', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-10'},
-    {'route' : 'Central Mediterranean', 'location' : 'Tunisia', 'number dead' : '8', 'number missing' : '15', 'date' : '2022-09-09'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Crete, Greece', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-08'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Lebanon', 'number dead' : '30', 'number missing' : '0', 'date' : '2022-04-24'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Turkey', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-08-26'},
-    {'route' : 'Western Mediterranean', 'location' : 'Canary Islands, Fuerteventura', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-08-16'},
-    {'route' : 'Central Mediterranan', 'location' : 'Libya, Sudan border', 'number dead' : '15', 'number missing' : '2', 'date' : '2022-08-15'},
-    {'route' : 'Western Mediterranean', 'location' : 'Algerian coast', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-08-09'},
-    {'route' : 'Western Mediterranean', 'location' : 'Morocco, Akhfennir', 'number dead' : '8', 'number missing' : '0', 'date' : '2022-07-26'},
-    {'route' : 'Sahara Desert crossing', 'location' : 'Niger, Libyan border', 'number dead' : '10', 'number missing' : '0', 'date' : '2022-07-01'},
-    {'route' : 'Central Mediterranean', 'location' : 'Calabrian coast', 'number dead' : '5', 'number missing' : '0', 'date' : '2022-07-25'},
-    {'route' : 'Sahara Desert crossing', 'location' : 'Libya, Chad border', 'number dead' : '20', 'number missing' : '0', 'date' : '2022-06-30'},
-    {'route' : 'Central Mediterranean', 'location' : 'Central Mediterranean', 'number dead' : '0', 'number missing' : '30', 'date' : '2022-06-27'},
-    {'route' : 'US-Mexico border crossing', 'location' : 'US, San Antonio', 'number dead' : '46', 'number missing' : '0', 'date' : '2022-06-28'},
-    {'route' : 'Central Mediterranean', 'location' : 'Morocco (to Mellilla)', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-06-28'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Lesbos, Greece', 'number dead' : '2', 'number missing' : '0', 'date' : '2022-06-27'},
-    {'route' : 'Central Mediterranean', 'location' : 'Tunisia, Coast of Sfax', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-06-21'},
-    {'route' : 'Eastern Mediterranean', 'location' : 'Greece, Aegean sea', 'number dead' : '0', 'number missing' : '8', 'date' : '2022-06-20'},
-    {'route' : 'Western Mediterranean', 'location' : 'Spain coast', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-06-09'},
-    {'route' : 'Caribbean to US', 'location' : 'Ilsa Morada', 'number dead' : '0', 'number missing' : '1', 'date' : '2022-09-02'}]
+    {'route' : 'Central Mediterranean', 'location' : 'Mahdia coast, Tunisia', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-14', 'lat' : '35.513919', 'lon' : '11.090457'},
+    {'route' : 'Central Mediterranean', 'location' : 'Trapani, Sicily', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-10-13', 'lat' : '38.034506', 'lon' : '12.476485'},
+    {'route' : 'Central Mediterranean', 'location' : 'Zarzis, Tunisia', 'number dead' : '11', 'number missing' : '0', 'date' : '2022-09-21', 'lat' : '33.509019', 'lon' : '11.134580'},
+    {'route' : 'Central Mediterranean', 'location' : 'Sbaratha, Libya', 'number dead' : '15', 'number missing' : '0', 'date' : '2022-10-07', 'lat' : '32.874355', 'lon' : '12.461700' },
+    {'route' : 'Eastern Mediterranean', 'location' : 'Kythira, Greece', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-10-05', 'lat' : '36.245586', 'lon' : '23.161616'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Lesbos', 'number dead' : '16', 'number missing' : '0', 'date' : '2022-10-06', 'lat' : '39.239494', 'lon' : '26.517556'},
+    {'route' : 'Western Mediterranean', 'location' : 'Spain / Canary Islands', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-10-06', 'lat' : '28.652163', 'lon' : '-15.661686'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Syria, near Arida Border', 'number dead' : '97', 'number missing' : '0', 'date' : '2022-09-26', 'lat' : '34.633319', 'lon' : '35.973976'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Aegean Sea, Turkey', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-15', 'lat' : '38.083909', 'lon' : '25.454807'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Turkey, coast of Izmir', 'number dead' : '6', 'number missing' : '5', 'date' : '2022-09-15', 'lat' : '38.437494', 'lon' : '27.070782'},
+    {'route' : 'Western Mediterranean', 'location' : 'Morocco, Tarfaya', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-13', 'lat' : '27.959682', 'lon' : '-12.933955'},
+    {'route' : 'Central Mediterranean', 'location' : 'Pozzallo, Sicily', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-09-10', 'lat' : '36.723665', 'lon' : '14.854199'},
+    {'route' : 'Central Mediterranean', 'location' : 'Tunisia, Chebba', 'number dead' : '8', 'number missing' : '15', 'date' : '2022-09-09', 'lat' : '35.238334', 'lon' : '11.184468'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Crete, Greece', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-09-08', 'lat' : '34.705937', 'lon' : '24.399454'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Lebanon, Tripoli', 'number dead' : '30', 'number missing' : '0', 'date' : '2022-04-24', 'lat' : '34.469548', 'lon' : '35.790515'},
+    {'route' : 'Iran to Türkiye', 'location' : 'Turkey, Van', 'number dead' : '1', 'number missing' : '0', 'date' : '2022-08-26', 'lat' : '38.502345', 'lon' : '43.235299'},
+    {'route' : 'Western Mediterranean', 'location' : 'Canary Islands, Fuerteventura', 'number dead' : '3', 'number missing' : '0', 'date' : '2022-08-16', 'lat' : '28.124122', 'lon' : '-13.638271'},
+    {'route' : 'Sahara Desert crossing', 'location' : 'Libya, North Darfur, Libya-Sudan border,', 'number dead' : '15', 'number missing' : '2', 'date' : '2022-08-15', 'lat' : '20.193175', 'lon' : '24.422461'},
+    {'route' : 'Western Mediterranean', 'location' : 'Algerian coast, Baïnem', 'number dead' : '6', 'number missing' : '0', 'date' : '2022-08-09', 'lat' : '36.824771', 'lon' : '2.969724'},
+    {'route' : 'Western Mediterranean', 'location' : 'Morocco, Akhfennir', 'number dead' : '8', 'number missing' : '0', 'date' : '2022-07-26', 'lat' : '28.099619', 'lon' : '-12.053869'},
+    {'route' : 'Sahara Desert crossing', 'location' : 'Niger, near Dirkou', 'number dead' : '10', 'number missing' : '0', 'date' : '2022-07-01', 'lat' : '20.023729', 'lon' : '13.161012'},
+    {'route' : 'Central Mediterranean', 'location' : 'Italy, 200km off the Calabrian coast', 'number dead' : '5', 'number missing' : '0', 'date' : '2022-07-25', 'lat' : '36.856427', 'lon' : '17.276735'},
+    {'route' : 'Sahara Desert crossing', 'location' : 'Libya, Kufra, Libya-Chad border', 'number dead' : '20', 'number missing' : '0', 'date' : '2022-06-30', 'lat' : '21.253623', 'lon' : '21.408366'},
+    {'route' : 'Central Mediterranean', 'location' : 'Central Mediterranean', 'number dead' : '1', 'number missing' : '22', 'date' : '2022-06-27', 'lat' : '34.844949', 'lon' : '17.664421'},
+    {'route' : 'US-Mexico border crossing', 'location' : 'US, San Antonio', 'number dead' : '46', 'number missing' : '0', 'date' : '2022-06-28', 'lat' : '29.385168', 'lon' : '-98.508756'},
+    {'route' : 'Central Mediterranean', 'location' : 'Morocco, Melilla Border)', 'number dead' : '23', 'number missing' : '0', 'date' : '2022-06-28', 'lat' : '35.288345', 'lon' : '-2.959516'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Greece, From Türkiye to Lesbos,', 'number dead' : '2', 'number missing' : '0', 'date' : '2022-06-27', 'lat' : '39.247488', 'lon' : '26.588229'},
+    {'route' : 'Central Mediterranean', 'location' : 'Tunisia, Skhira', 'number dead' : '12', 'number missing' : '0', 'date' : '2022-06-20', 'lat' : '34.285009', 'lon' : '10.107038'},
+    {'route' : 'Central Mediterranean', 'location' : 'Tunisia, Coast of Sfax', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-06-21', 'lat' : '34.687726', 'lon' : '10.814120'},
+    {'route' : 'Eastern Mediterranean', 'location' : 'Greece, Aegean sea, 2 miles southeast of Delos', 'number dead' : '0', 'number missing' : '8', 'date' : '2022-06-20', 'lat' : '37.370551', 'lon' : '25.296742'},
+    {'route' : 'Western Mediterranean', 'location' : 'Spain, near Escombreras', 'number dead' : '4', 'number missing' : '0', 'date' : '2022-06-09', 'lat' : '37.537481', 'lon' : '-0.910363'},
+    {'route' : 'Caribbean to US', 'location' : 'Islamorada', 'number dead' : '0', 'number missing' : '1', 'date' : '2022-09-02', 'lat' : '24.903589', 'lon' : '-80.608638'}]
 
-s = defaultdict(int)
 
-# counts for each route
-for d in data_list:
-    string = d['route']
-    s[string] += 1
+tweet_df = pd.DataFrame.from_dict(data_list)
 
-print(s)
+tweet_df['date'] = pd.to_datetime(tweet_df['date'])
+tweet_df['number dead'] = tweet_df['number dead'].astype(int)
+tweet_df['number missing'] = tweet_df['number missing'].astype(int)
 
-dataframe = pd.DataFrame.from_dict(data_list)
+# data after August
+tweet_df = tweet_df.loc[(tweet_df['date'] >= '2022-08-01')]
 
 # save dataframe to csv
-#dataframe.to_csv('data.csv', index=False)
+tweet_df.to_csv('data.csv', index = False)
--- src/pages/Recent_data_from_Twitter.py
+++ src/pages/Recent_data_from_Twitter.py
@@ -3,7 +3,6 @@ import streamlit.components.v1 as components
 import requests
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 
 def get_data():
     return pd.read_csv("../data/data.csv")
@@ -12,14 +11,20 @@ tweet_df = get_data()
 
 def plot_deaths_month(route, df):
     route_df = df[df['route'] == route]
-    fig = px.bar(route_df, x='date', y=['number dead','number missing'], title=f"Plot of deaths/missing by month along the {route} route.")
-    fig.update_xaxes(range=['2022-08-01','2022-11-01'])
-    fig.update_traces(width=100000000)
+    fig = px.bar(
+        route_df,
+        x = 'date',
+        y = ['number dead', 'number missing'],
+        title = f"Plot of deaths/missing by month along the {route} route."
+        )
+
+    fig.update_xaxes(range = ['2022-08-01', '2022-11-01'])
+    fig.update_traces(width = 100000000)
     fig.update_layout({
-        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-        'xaxis_title':"Date", 
-        'yaxis_title':"Number of Deaths/Missing"
+        'plot_bgcolor' : 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor' : 'rgba(0, 0, 0, 0)',
+        'xaxis_title' : "Date", 
+        'yaxis_title' : "Number of Deaths/Missing"
     })
     st.write(fig)
 
@@ -29,14 +34,50 @@ def tweet_embed(tweet_url):
     rjs = resp.json()['html']
     return rjs
 
-# data after August
-tweet_df = tweet_df.loc[(tweet_df['date'] >= '2022-08-01')]
 tweet_df['date'] = pd.to_datetime(tweet_df['date'])
-
+tweet_df['date_dmy'] = tweet_df['date'].dt.strftime('%d/%m/%Y')
 tweet_df['number dead'] = tweet_df['number dead'].astype(int)
 tweet_df['number missing'] = tweet_df['number missing'].astype(int)
 
 df1 = tweet_df.groupby(['route','date'])[['number dead', 'number missing']].sum().reset_index()
+
+plotdf = tweet_df[['lat', 'lon']]
+plotdf['Migration Route'] = tweet_df['route']
+plotdf['Date'] = tweet_df['date_dmy']
+plotdf['Number of People Dead'] = tweet_df['number dead']
+plotdf['Number of People Missing'] = tweet_df['number missing']
+
+plot = px.scatter_geo(
+    plotdf,
+    lat = 'lat',
+    lon = 'lon',
+    hover_name = 'Migration Route',
+    hover_data = {'Number of People Dead' : True,
+                'Number of People Missing' : True,
+                'Date' : True,
+                'lon' : False,
+                'lat' : False
+                },
+    color_discrete_sequence = ['red']
+    )
+
+plot.update_layout(
+    autosize = False,
+    geo = dict(
+        scope = 'world',
+        showcountries = True,
+        showocean = True,
+        landcolor = '#f2f2f0',
+        oceancolor = '#cad2d3',
+        showcoastlines = False,
+    ),
+    margin = {
+        "r" : 0,
+        "t" : 0,
+        "l" : 0,
+        "b" : 0
+        },
+)
 
 # Page: Twitter Integration
 st.header("Twitter API Integration for More Recent Data")
@@ -56,17 +97,20 @@ example_tweet = tweet_embed(t_link)
 components.html(example_tweet, height=500)
 
 st.markdown('After downloading the Tweets, we filtered the ones relating to incidents by choosing ones containing keywords such as migrant, missing, \
-etc. From reading these and writing down data, we were able to obtain a dataset of recent incidents containing the number of people dead/missing, \
+etc. From reading these and writing down the data, we were able to obtain a dataset of recent incidents containing the number of people dead/missing, \
 location/migratory route, and the date when it was reported for each incident.')
 
 st.markdown('Below is an example of what the dataset looks like.')
-st.dataframe(df1.head(7))
+st.dataframe(df1.head(8))
 
 st.markdown('Nearly all of the data was from the Mediterranean. This is likely due to fact that according to data from the Missing Migrants Project, \
 Mediterranean routes are the ones with the most incidents, and can also partly be because one the sources was [@SARwatchMED](https://twitter.com/SARwatchMED), \
 which exclusively reports on search-and-rescue operations on the Mediterranean.')
 
-st.markdown('From the Data Filters in the left menu, you can choose the route you want to see the data for. The graph below shows the number of deaths and missing. ')
+st.markdown('Below is a map of the different incidents from the Twitter data.')
+st.plotly_chart(plot)
+
+st.markdown('Each incident is plotted in the below graph showing the number of people dead or missing. From the Data Filters in the left menu, you can choose the route you want to see the data for.')
 
 st.sidebar.write("Data Filters")
 route_input = [st.sidebar.selectbox(
